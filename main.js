c3377c86e05266e497a96f1964ba37c3ac25e377
Janelle Salisbury <janellesalisbury@fullsail.edu>
2011-10-24 18:43:22 -0400

    this isnt that bad after all

--- main.js
+++ main.js
@@ -4,49 +4,44 @@
 
 window.addEventListener("DOMContentLoaded", function(){
 
+//get element by id function
 	function $(x){
 	   var theElement = document.getElementById(x);
 	   return theElement;
 	}
+	
+// create select field and populate with options
+	function makeCats(){
+		var formTag = document.getElementsByTagName("form"),
+			selectLi = ("select"),
+			makeSelect = document.createElement("select");
+			makeSelect.setAttribute("id","groups");
+		for (var i = 0, j = contactGroups.length; i<j; i++){
+			var makeOption = document.createElement("option");
+			var optText = contactGroups[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);
+	
+	
+	}
+
+//Variable defaults
+	var clothingCategories = [--"Choose a Category"--, "Shoes", "Tops", "Bottoms", "Jewelry", "Handbags"];
+	
+
+//set link & submit click events
+
+	var displayLink = $("displayLink");
+	displayLink.addEventListener("click", getData);
+	var clearLink = $("clear");
+	clearLink.addEventListener("click", clearLocal);
+	var save = $("submit");
+	save.addEventListener("click", storeData);
+	
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+	};
