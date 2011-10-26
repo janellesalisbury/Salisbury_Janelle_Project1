3fb25c4ec2e6cbd1eb0d65b693265e0d64885091
Janelle Salisbury <janellesalisbury@fullsail.edu>
2011-10-26 13:56:43 -0400

    getting good at this

--- js/main.js
+++ js/main.js
@@ -82,12 +82,12 @@ window.addEventListener("DOMContentLoaded", function(){
 	//write data from local storage to browser
 		var makeDiv = document.createElement("div");
 		makeDiv.setAttribute("id", "items");
-		var makeList = document.createElement ("ul");
+		var makeList = document.createElement("ul");
 		makeDiv.appendChild(makeList);
 		document.body.appendChild(makeDiv);
 		$("items").style.display = "block";
 		for (var i=0, len=localStorage.length; i<len;i++){
-			var makeLi = document.createElement ("li");
+			var makeLi = document.createElement("li");
 			var linksLi = document.createElement("li");
 			makeList.appendChild(makeLi);
 			var key = localStorage.key(i);
@@ -95,18 +95,76 @@ window.addEventListener("DOMContentLoaded", function(){
 			// convert the string from local storage value back to an object using JSON.parse ()
 			var obj = JSON.parse(value);
 			var makeSubList = document.createElement("ul");
-			makeli.appendChild(makeSubList);
+			makeLi.appendChild(makeSubList);
 			for (var n in obj){
 				var makeSubli=document.createElement("li");
 				makeSubList.appendChild(makeSubli);
 				var optSubText = obj[n][0]+" "+obj[n][1];
 				makeSubli.innerHTML = optSubText;
-				makeSubList.appendChild(linksli);
-				makeSubList.appendChild(linksli);
+				makeSubList.appendChild(linksLi);
+				
 			}	
-		//makeItemLinks(); create our edit and delete buttons/links for each item in local storage.
+		makeItemLinks(localStorage.key(i), linksLi); //create our edit and delete buttons/links for each item in local storage.
 		}
 	}
+	
+	//Make Item Links
+	//Create the edit and delete links for each stored item when displayed
+	
+	function makeItemLinks(key, linksLi){
+	//add edit single item link
+	var editLink = document.createElement("a");
+    editLink.href = "#";
+	editLink.key = key;
+	var editText = "Edit Item";
+	editLink.addEventListener("click", editItem);
+	editLink.innerHTML = editText;
+	linksLi.appendChild(editLink);
+	
+	//add link break
+	var breakTag = document.createElement("br");
+	linksLi.appendChild(breakTag);
+	
+	
+	//add delete single item link
+	var deleteLink = document.createElement("a");
+	deleteLink.href = "#";
+	deleteLink.key = key;
+	var deleteText = "Delete Item";
+	//deleteLink.addEventListener("click", deleteItem);
+	deleteLink.innerHTML = deleteText;
+	linksLi.appendChild(deleteLink);
+	
+	}
+	
+	function editItem(){
+		// Grab the data from our item from local storage.
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+		//show the form
+		toggleControls("off");
+		
+		// Populate the form fields with the current local storage values.
+		$("groups").value = item.group[1];
+		$("name").value = item.name[1];
+		$("brand").value = item.brand[1];
+		$("color").value = item.color[1];
+		if(item.dryClean[1]=="Yes"){
+			$("dryCleanOnly").setAttribute("checked", "checked");
+		}
+		$("notes").value = item.notes[1];
+		
+		//Remove the initial listener from the input 'save item' button.
+		save.removeEventListener("click", storeData);
+		// Change submit button value to Edit Button.
+		$("submit").value = "Edit Item";
+		var editSubmit = $("submit");
+		editSubmit.addEventListener("click", validate);
+		editSubmit.key = this.key;
+	
+	
+	}
+	
 	
 	function clearLocal(){
 		if (localStorage.length === 0){
@@ -119,7 +177,44 @@ window.addEventListener("DOMContentLoaded", function(){
 			return false;
 		}
 	}
-
+	
+	function validate(){
+	// Define elements we want to check
+		var getGroup = $("groups");
+		var getName = $("name");
+		var getBrand = $("brand");
+		var getColor = $("color");
+		
+		//Get error messages
+		var messageAry = [];
+		//Group validation
+		if(getGroup === "--- Choose A Group ---"){
+			var groupError = "Please choose a group";
+			getGroup.style.border = "1px solid red";
+			messageAry.push(groupError);
+		}	
+		//Name
+		if(getName.value === ""){
+			var nameError = "Please enter a name";
+			getName.style.border = "1px solid red";
+			messageAry.push(nameError);
+		}	
+		//Brand 
+		if(getBrand.value === ""){
+			var brandError = "Please enter a brand";
+			getBrand.style.border = "1px solid red";
+			messageAry.push(brandError);
+		}
+		//Color
+		if(getColor.value === ""){
+			var colorError = "Please enter a color";
+			getColor.style.border = "1px solid red";
+			messageAry.push(colorError);
+		}
+		storeData();
+		
+		
+	}
 //Variable defaults
 	var clothingCategories = ["--Choose a Category--", "Shoes", "Tops", "Bottoms", "Jewelry", "Handbags"],
 		dryCleanValue = "No"
@@ -134,7 +229,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	clearLink.addEventListener("click", clearLocal);
 	var save = $("submit");
 	console.log(save);
-	save.addEventListener("click", storeData);
+	save.addEventListener("click", validate);
 	
 
 
