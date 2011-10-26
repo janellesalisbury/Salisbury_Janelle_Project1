f47768ce7b53b4bbd15d7771d525f0ada14b3011
Janelle Salisbury <janellesalisbury@fullsail.edu>
2011-10-25 20:23:50 -0400

    found some errors, just working on getting the videos completed and then fixing errors.

--- js/main.js
+++ js/main.js
@@ -36,6 +36,27 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 	}
 	
+	function toggleControls(n){
+		switch(n){
+			case "on":
+				$("contactForm").style.display = "none";
+				$("clear").style.display = "inline";
+				$("displayLink").style.display = "none";
+				$("addNew").style.display = "inline";
+				break;
+			case "off":
+				$("contactForm").style.display = "block";
+				$("clear").style.display = "inline";
+				$("displayLink").style.display = "inline";
+				$("addNew").style.display = "none";
+				$("items").style.display = "none";
+				break;
+			default:
+				return false;
+		}
+			
+	}
+	
 	function storeData(){
 		var id		= Math.floor(Math.random()*100000001);
 		// Gather up form field values and store in an object
@@ -52,6 +73,39 @@ window.addEventListener("DOMContentLoaded", function(){
 		localStorage.setItem(id, JSON.stringify(item));
 		alert ("Item Saved");
 	}
+	
+	function getData(){
+	//write data from local storage to browser
+		var makeDiv = document.createElement("div");
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement ("ul");
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$("items").style.display = "block";
+		for (var i=0, len=localStorage.length; i<len;i++){
+			var makeLi = document.createElement ("li");
+			makeList.appendChild(makeLi);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			// convert the string from local storage value back to an object using JSON.parse ()
+			var obj = JSON.parse(value);
+			var makeSubList = document.createElement("ul");
+			makeli.appendChild(makeSubList);
+			for (var n in obj){
+				var makeSubli=document.createElement("li");
+				makeSubList.appendChild(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.innerHTML = optSubText;
+			}	
+		
+		}
+	}
+	
+	
+	
+	
+	
+	}
 
 //Variable defaults
 	var clothingCategories = ["--Choose a Category--", "Shoes", "Tops", "Bottoms", "Jewelry", "Handbags"],
@@ -61,8 +115,8 @@ window.addEventListener("DOMContentLoaded", function(){
 
 //set link & submit click events
 
-	//var displayLink = $("displayLink");
-	//displayLink.addEventListener("click", getData);
+	var displayLink = $("displayLink");
+	displayLink.addEventListener("click", getData);
 	var clearLink = $("clear");
 	//clearLink.addEventListener("click", clearLocal);
 	var save = $("submit");
